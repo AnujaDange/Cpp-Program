8480dc8c0f8d1c493266550b263c694bdcc8be67
AnujaDange <92947707+AnujaDange@users.noreply.github.com>
2022-08-22 20:36:54 +0530

    Add files via upload

---
+++ Pattern Printing/program to print Rhombus pattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class pattern
+{
+public:
+    int i,j,r;
+    void rhombus()
+    {
+        cout<<"Enter row size=> ";
+        cin>>r;
+        for(i=1; i<=r; i++)
+        {
+           for(j=1; j<=r-i; j++)
+           {
+               cout<<"  ";
+           }
+            for(j=1; j<=r; j++)
+           {
+              cout<<"*";
+           }
+            cout<<"\n";
+        }
+    }
+};
+int main()
+{
+    pattern s;
+    s.rhombus();
+    return(0);
+}
